97650bfa2b3524401d2d10924707f2800df5b6d3
wuketao <wuketao@houbank.com>
2020-03-29 15:30:26 +0800

    init

---
+++ src/main/java/com/test/Application.java
@@ -0,0 +1,16 @@
+package com.test;
+
+import com.alibaba.fastjson.JSON;
+
+import java.lang.reflect.Array;
+
+/**
+ * @author wuketao
+ * @date 2020/2/16
+ * @Description
+ */
+public class Application {
+    public static void main(String[] args) {
+    }
+
+}
