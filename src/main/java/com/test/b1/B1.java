fb530ca3971614bd090a018387a863305e522cde
wuketao <wuketao@houbank.com>
2020-03-29 15:58:05 +0800

    b1 delete old file and add B1.java

--- src/main/java/com/test/b1/B1.java
+++ src/main/java/com/test/b1/B1.java
@@ -1,9 +1,9 @@
-package com.test.t2;
+package com.test.b1;
 
 /**
  * @author wuketao
  * @date 2020/3/29
  * @Description
  */
-public class Test2 {
+public class B1 {
 }
--- src/main/java/com/test/t3/T3.java
+++
@@ -1,9 +0,0 @@
-package com.test.t3;
-
-/**
- * @author wuketao
- * @date 2020/3/29
- * @Description
- */
-public class T3 {
-}
